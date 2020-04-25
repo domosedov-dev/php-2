6f2be9780674353a1e368966101611b3ea06dd27
Aleksandr Grigorii <42298502+domosedov-dev@users.noreply.github.com>
2020-04-25 21:58:22 +0300

    Update Index.php

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -12,6 +12,12 @@ class Index extends Controller
 
     protected function handle()
     {
+        /* 
+        * Здесь идет обращение к методам из трейта Magic,
+        * который в массив data с ключем 'articles' передают данные Article::findAll()
+        * Затем уже в шаблоне мы можем обратиться к data, c помощью $this->articles,
+        * что по сути является $this->data['articles'];
+        */
         $this->view->articles = Article::findAll();
         $this->view->display(__DIR__ . '/../View/templates/news.php');
     }
